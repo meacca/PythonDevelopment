d46871348eabff5ead4039491b7418f5a514e962
meacca <dimitar98@list.ru>
2021-03-12 02:29:38 +0300

    Added number buttons resizeblness

--- 03_ThreeWayAndTkinter/puzzle_15.py
+++ 03_ThreeWayAndTkinter/puzzle_15.py
@@ -7,7 +7,15 @@ def generate_dummy_number_buttons(window):
         column = i % 4
         text = str(i + 1)
         new_number = tk.Button(window, text=text)
-        new_number.grid(row=row, column=column)
+        new_number.grid(row=row, column=column, sticky="NEWS")
+
+
+def configure_row_columns(window):
+    for column in range(0, 4):
+        window.columnconfigure(column, weight=1)
+    for row in range(1, 5):
+        window.rowconfigure(row, weight=1)
+
 
 
 main_window = tk.Tk()
@@ -16,10 +24,9 @@ new_button = tk.Button(main_window, text="New")
 new_button.grid(row=0, column=0, columnspan=2, sticky="N")
 exit_button = tk.Button(main_window, text='Exit')
 exit_button.grid(row=0, column=2, columnspan=2, sticky="N")
-main_window.columnconfigure(0, weight=1)
-main_window.columnconfigure(1, weight=1)
 
 generate_dummy_number_buttons(main_window)
+configure_row_columns(main_window)
 
 main_window.title("15")
 main_window.mainloop()
