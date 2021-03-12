d19690971a54cb70812dc6e0e08ffc062cc17bd0
meacca <dimitar98@list.ru>
2021-03-12 03:20:18 +0300

    added move number functionality

--- 03_ThreeWayAndTkinter/puzzle_15.py
+++ 03_ThreeWayAndTkinter/puzzle_15.py
@@ -3,6 +3,7 @@ import random
 
 
 def generate_random_number_buttons():
+    global current_empty
     random_empty = random.randint(0, 15)
     random_seq = list(range(0, 15))
     random.shuffle(random_seq)
@@ -13,6 +14,7 @@ def generate_random_number_buttons():
         if i != random_empty:
             number_buttons[random_seq[cur_elem]].grid(row=row, column=column, sticky="NEWS")
             cur_elem += 1
+    current_empty = random_empty
 
 
 def configure_row_columns():
@@ -22,8 +24,21 @@ def configure_row_columns():
         main_window.rowconfigure(row, weight=1)
 
 
+def number_click_action(number_idx):
+    def custom_func():
+        global current_empty
+        empty_row, empty_column = current_empty // 4 + 1, current_empty % 4
+        cur_row = number_buttons[number_idx].grid_info()["row"]
+        cur_column = number_buttons[number_idx].grid_info()["column"]
+        if abs(cur_row - empty_row) + abs(cur_column - empty_column) <= 1:
+            number_buttons[number_idx].grid(row=empty_row, column=empty_column, sticky="NEWS")
+            current_empty = (cur_row - 1) * 4 + cur_column % 4
+    return custom_func
+
+
 main_window = tk.Tk()
-number_buttons = [tk.Button(main_window, text=str(i+1)) for i in range(15)]
+current_empty = 0
+number_buttons = [tk.Button(main_window, text=str(i+1), command=number_click_action(i)) for i in range(15)]
 
 generate_random_number_buttons()
 configure_row_columns()
