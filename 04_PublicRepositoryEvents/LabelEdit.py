c9a620a4bc54c526ed787f45d3952bc7601747e8
meacca <dimitar98@list.ru>
2021-03-22 23:14:48 +0300

    added other keys reactions:

--- 04_PublicRepositoryEvents/LabelEdit.py
+++ 04_PublicRepositoryEvents/LabelEdit.py
@@ -5,16 +5,19 @@ class InputLabel(tk.Label):
     def __init__(self, master):
         self.S = tk.StringVar("")
         self.fontsize = 10
-        super().__init__(master, textvar=self.S, font=("Courier", self.fontsize+2), relief="sunken")
+        super().__init__(master, textvar=self.S, font=("TkFixedFont", self.fontsize), relief="sunken",
+                         highlightthickness=1)
         self.F = tk.Frame(self, height=20, width=1, bg="black")
 
         self.cursor_position = 0
         self.bind("<KeyPress>", self.keypress_handler)
         self.bind("<Button-1>", self.buttonpress_handler)
+        self.focus()
 
     def move_cursor(self, new_pos):
         self.cursor_position = max(min(new_pos, len(self.S.get())), 0)
         self.F.place(x=self.fontsize * self.cursor_position)
+        self.focus()
 
     def add_symbol(self, char):
         old_string = self.S.get()
@@ -22,9 +25,25 @@ class InputLabel(tk.Label):
         self.S.set(new_string)
         self.move_cursor(self.cursor_position+1)
 
+    def delete_symbol(self):
+        old_string = self.S.get()
+        new_string = old_string[:max(self.cursor_position - 1, 0)] + old_string[self.cursor_position:]
+        self.S.set(new_string)
+        self.move_cursor(self.cursor_position-1)
+
     def keypress_handler(self, event):
         if event.char.isprintable():
             self.add_symbol(event.char)
+        elif event.keysym == "Right":
+            self.move_cursor(self.cursor_position+1)
+        elif event.keysym == "Left":
+            self.move_cursor(self.cursor_position-1)
+        elif event.keysym == "Home":
+            self.move_cursor(0)
+        elif event.keysym == "End":
+            self.move_cursor(len(self.S.get()))
+        elif event.keysym == "BackSpace":
+            self.delete_symbol()
 
     def buttonpress_handler(self, event):
         self.focus()
